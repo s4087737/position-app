b2940dffa81db108ac6a008648565dcf30a35368
Joel <lotrjoel@gmail.com>
2024-10-25 19:25:44 +1100

    Final Draft

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,7 @@ const config = {
     kit: {
         adapter: adapter(),
         paths: {
-            base: process.env.NODE_ENV === 'production' ? '/lotrjoel-position-app' : '',
+            base: process.env.NODE_ENV === 'production' ? '/position-app' : '',
         },
     },
     preprocess: vitePreprocess(),
